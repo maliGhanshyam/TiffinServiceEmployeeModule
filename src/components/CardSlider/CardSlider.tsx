2839ba3a1b89f3cb66d97845fcc602e0907bd8e4
surajraul <surajraul98@gmail.com>
2024-11-29 11:56:17 +0530

    ads on card slider setting change

--- src/components/CardSlider/CardSlider.tsx
+++ src/components/CardSlider/CardSlider.tsx
@@ -5,31 +5,46 @@ import { CardSliderProps } from "./CardSlider.types";
 import { styles } from "./CardSlider.styles";
 
 const CardSlider = <T extends { _id: string }>({ data, children, settings }: CardSliderProps<T>) => {
+  const isSingleItem = data.length === 1;
   const defaultSettings = {
-    infinite: true,
+    infinite: false,
     speed: 500,
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 2,
     dots: true,
     centerMode: false,
-    
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
+          centerMode: false,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
           centerMode: false,
         },
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
+          slidesToShow: 2,
+          slidesToScroll: 2,
           centerMode: false,
         },
       },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
     ],
     ...settings,
   };
